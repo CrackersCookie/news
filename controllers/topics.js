b68788830608ceaf99df92ddae85d85d36ef8eb4
Mark Cooke <crackerscookie@yahoo.com>
2019-07-15 16:51:01 +0100

    added topics controller main controller folder

---
+++ controllers/topics.js
@@ -0,0 +1,10 @@
+const { selectTopics } = require('../models/topics')
+
+
+const sendTopics = (req, res, next) => {
+  selectTopics().then((topics) => {
+    res.status(200).send({ topics })
+  })
+}
+
+module.exports = { sendTopics }
--- routes/topicsRouter.js
+++ routes/topicsRouter.js
@@ -1,6 +1,6 @@
 const topicsRouter = require('express').Router();
-
+const { sendTopics } = require('../controllers/topics')
 
 topicsRouter.route('/').get(sendTopics)
 
-module.exports = apiRouter
+module.exports = topicsRouter

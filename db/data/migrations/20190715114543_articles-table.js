011f1b4c7d2443332370ec481f3d6747174b89c8
Mark Cooke <crackerscookie@yahoo.com>
2019-08-06 16:50:34 +0100

    making small edits to the delete articles functionality

--- db/data/migrations/20190715114543_articles-table.js
+++ db/data/migrations/20190715114543_articles-table.js
@@ -5,8 +5,8 @@ exports.up = function (connection) {
     articlesTable.string('title').notNullable();
     articlesTable.string('body', 5000).notNullable();
     articlesTable.integer('votes').defaultTo(0);
-    articlesTable.string('topic').references('topics.slug').notNullable();
-    articlesTable.string('author').references('users.username').notNullable();
+    articlesTable.string('topic').references('topics.slug').notNullable().onDelete("CASCADE");
+    articlesTable.string('author').references('users.username').notNullable().onDelete("CASCADE");
     articlesTable.timestamp('created_at').defaultTo(connection.fn.now());
   })
 };
